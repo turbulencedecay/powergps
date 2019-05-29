b9747017d50f97b36c6e391a1bc7883203824ca0
turbulencedecay <uzunovic@gmx.net>
2019-05-29 15:57:13 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -11,11 +11,10 @@ import fitparse, dateutil, pytz
 from lxml import objectify
 import pandas as pd
 import numpy as np
-#import geopy.distance
+from geopy.distance import geodesic
 import matplotlib.pyplot as plt
 #import scipy as sc
 #from scipy import optimize
-#from scipy import signal
 
 UTC = pytz.UTC
 BER = pytz.timezone('Europe/Berlin')
@@ -38,15 +37,15 @@ def GetTcxData(tcxFileName):
             else:
                 point[item] = float('nan')
         if hasattr(tp, 'Position'):
-            point['Position.LatitudeDegrees'] = tp.Position.LatitudeDegrees.text
-            point['Position.LongitudeDegrees'] = tp.Position.LongitudeDegrees.text
+            point['Position_LatitudeDegrees'] = tp.Position.LatitudeDegrees.text
+            point['Position_LongitudeDegrees'] = tp.Position.LongitudeDegrees.text
         else:
-            point['Position.LatitudeDegrees'] = float('nan')
-            point['Position.LongitudeDegrees'] = float('nan')
+            point['Position_LatitudeDegrees'] = float('nan')
+            point['Position_LongitudeDegrees'] = float('nan')
         if hasattr(tp, 'HeartRateBpm'):
-            point['HeartRateBpm.Value'] = tp.HeartRateBpm.Value.text
+            point['HeartRateBpm_Value'] = tp.HeartRateBpm.Value.text
         else:
-            point['HeartRateBpm.Value'] = float('nan')
+            point['HeartRateBpm_Value'] = float('nan')
         tcxData.append(point)
     for item in tcxData:
         item['Time'] = dateutil.parser.parse(item['Time'])
@@ -65,7 +64,7 @@ fitData = GetFitData(fitFileName)
 
 fitData = pd.DataFrame(fitData)
 tcxData = pd.DataFrame(tcxData)
-tcxData = tcxData.fillna(value=-1)
+#tcxData = tcxData.fillna(value=-1)
 tcxData = tcxData.apply(pd.to_numeric)
 tcxData.Time = tcxData.Time.apply(pd.to_datetime)
 
@@ -87,20 +86,37 @@ resampledFitData = resampledFitData.loc[tcxData.index]
 for item in(resampledFitData.columns):
     tcxData = tcxData.join(resampledFitData[item])
 
-tcxData = tcxData.drop('heart_rate', axis=1) # empty
-tcxData = tcxData.drop('position_lat', axis=1) # empty
-tcxData = tcxData.drop('position_long', axis=1) # empty
+tcxData = tcxData.drop('heart_rate', axis=1) # empty, footpod
+tcxData = tcxData.drop('position_lat', axis=1) # empty, footpod
+tcxData = tcxData.drop('position_long', axis=1) # empty, footpod
 tcxData = tcxData.drop('speed', axis=1) #speed in m/h
-#tcxData = tcxData.drop('Speed', axis=1) #speed in m/s
 tcxData = tcxData.drop('enhanced_speed', axis=1) #speed in km/h
 tcxData = tcxData.drop('distance', axis=1) #distance in km
+tcxData = tcxData.drop('cadence', axis=1) # footpod cadence
+tcxData = tcxData.drop('altitude', axis=1) # what the hell is this, footpod
+
+#   fill missing AltitudeMeters
+tcxData.AltitudeMeters=tcxData.AltitudeMeters.fillna(method='backfill')
+
+#   fill missing DistanceMeters
+tcxData.DistanceMeters=tcxData.DistanceMeters.interpolate(method='linear', limit_area='inside')
+tcxData.DistanceMeters=tcxData.DistanceMeters.interpolate(method='linear', limit_area='outside')
+
+#   fill missing AltitudeMeters
+tcxData.Position_LongitudeDegrees=tcxData.Position_LongitudeDegrees.interpolate(method='linear', limit_area='inside')
+tcxData.Position_LongitudeDegrees=tcxData.Position_LongitudeDegrees.interpolate(method='linear', limit_area='outside')
+tcxData.Position_LatitudeDegrees=tcxData.Position_LatitudeDegrees.interpolate(method='linear', limit_area='inside')
+tcxData.Position_LatitudeDegrees=tcxData.Position_LatitudeDegrees.interpolate(method='linear', limit_area='outside')
+
+
 
 def GetPace():
     # speed in m/s
     decimalPace = 1000 / tcxData.Speed.get_values() # min/km
     decimalPace[decimalPace == np.inf] = 0
     minutes, seconds = divmod(decimalPace, 60)
-    return (minutes,seconds)
+    paceElements = np.array((np.abs(minutes),np.round(seconds)))
+    return paceElements.T
 
 #y1 = np.vstack((tcxTime.get_values(), tcxData.Cadence.get_values()))
 #y2 = np.vstack((fitTime.get_values(), resampledFitData.cadence.get_values()))
@@ -124,10 +140,30 @@ def GetPace():
 #bestShift=bestShift-2
 
 if __name__ == '__main__':
-#    plt.plot_date(tcxData.index, tcxData.Speed, 'r-')
-#    plt.plot_date(tcxData.index, tcxData.speed/1000, 'b-')
+#    plt.plot_date(tcxData.index, tcxData.enhanced_altitude, 'r-')
+#    plt.plot_date(tcxData.index, tcxData.AltitudeMeters, 'b-')
+#    factor=(tcxData.enhanced_altitude-tcxData.AltitudeMeters).mean()
+#    plt.plot(tcxData.enhanced_altitude.get_values()-factor, 'r-')
+#    plt.plot(tcxData.AltitudeMeters.get_values(), 'b-')
+#    plt.plot(tcxData.power.get_values()/100, 'b-')
+#    plt.plot(tcxData.Speed.get_values(), 'r-')
 #    plt.show()
 #    plt.xlim(['2019-05-05 07:00:00+00:00', '2019-05-05 07:30:00+00:00'])
 #    a=GetPace()
-    pass
-#
+#    pass
+    firstLat = tcxData.Position_LatitudeDegrees[0::2].get_values()
+    secondLat = tcxData.Position_LatitudeDegrees[1::2].get_values()
+    firstLon = tcxData.Position_LongitudeDegrees[0::2].get_values()
+    secondLon = tcxData.Position_LongitudeDegrees[1::2].get_values()
+    firsts = np.array((firstLat, firstLon))
+    seconds = np.array((secondLat, secondLon))
+    d=[]
+    sum=0
+    for a,b in zip(firsts.T, seconds.T):
+        sum +=geodesic(a, b).meters
+        d.append(sum)
+    d=np.array(d)
+    plt.plot(tcxData.distance.get_values(),'-')
+    plt.plot(tcxData.DistanceMeters.get_values(), '-')
+    plt.show()
+#for item in 1
